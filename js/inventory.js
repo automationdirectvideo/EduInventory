bea1daa6472d139dfdeb8fcf5752ef5123ce99cd
rstrozensky <rstrozensky@automationdirect.com>
2019-07-17 15:27:07 -0400

    Load inventory table with sheets data

--- js/inventory.js
+++ js/inventory.js
@@ -17,3 +17,28 @@ function searchForPart(partNumber) {
     $('#inventory-table').DataTable().search(partNumber).draw();
   }
 }
+
+function loadTable() {
+  if (partData) {
+    let inventoryBody = document.getElementById("inventory-body");
+    for (let i = 0; i < partNumbers.length; i++) {
+      let partNumber = partNumbers[i];
+      let description = partData[partNumber]["description"];
+      let price = partData[partNumber]["price"];
+      let rowText = `
+        <tr id="row-${i}">
+          <th scope="row" id="part-number-row-${i}">${partNumber}</th>
+          <td id="quantity-row-${i}">#</td>
+          <td id="description-row-${i}">${description}</td>
+          <td id="price-row-${i}">${price}</td>
+          <td id="obsolete-row-${i}">No</td>
+          <td id="location-row-${i}"><a href="map.html"><i class="fa fa-map-marker-alt mr-1"></i>K4</a></td>
+        </tr>`;
+      var row = document.createElement("TR");
+      row.innerHTML = rowText;
+      row.id = "row-" + i;
+      inventoryBody.appendChild(row);
+    }
+    document.getElementById("row-#").remove();
+  }
+}
