f7dd407f454bae9540b2833fa3457f321b9cbb6a
rstrozensky <rstrozensky@automationdirect.com>
2019-07-18 10:11:32 -0400

    Add scanning to add multiple items page

--- js/add-multiple.js
+++ js/add-multiple.js
@@ -13,9 +13,51 @@ function loadPage() {
   loadAutocomplete();
 }
 
+function addPartToTable(partNumber) {
+  let partInputs = document.getElementsByClassName("part-input");
+  let index = 0;
+  let partFound = false;
+  while (index < partInputs.length && !partFound) {
+    if (partInputs[index].value == partNumber) {
+      partFound = true;
+    } else {
+      index++;
+    }
+  }
+  if (partFound) {
+    let partInput = partInputs[index];
+    let row = partInput.parentElement.parentElement;
+    let numItemsInput = row.cells[2].children[0];
+    numItemsInput.value = parseInt(numItemsInput.value) + 1;
+    updateRowQuantities(numItemsInput);
+  } else {
+    let row = createRow();
+    let partInput = row.cells[1].children[0];
+    partInput.value = partNumber;
+    let tbody = document.getElementById("parts-table-body");
+    tbody.prepend(row);
+    loadAutocomplete();
+    updateRowPartNumber(partInput);
+  }
+}
+
+function createRow() {
+  var rowText = `
+    <th scope="row"><i class="fa fa-minus-circle remove-icon" onclick="removeRow(this)"></i></th>
+    <td><input class="text-input part-input" type="text" name="part-number" oninput="updateRowPartNumber(this)"></td>
+    <td>
+      <input class="num-items" type="number" name="num-items" min="1" value="1" style="width:60px;" oninput="updateRowQuantities(this)">
+    </td>
+    <td>10</td>
+    <td>#</td>
+  `;
+  var row = document.createElement("TR");
+  row.innerHTML = rowText;
+  return row;
+}
+
 function displayCodeFromScan(code) {
-  let partSearch = document.getElementById("search-part-number");
-  partSearch.value = code;
+  addPartToTable(code);
 }
 
 function removeRow(elem) {
@@ -49,17 +91,7 @@ function updateRowQuantities(elem) {
 var addRowButton = document.getElementById("add-row-button");
 addRowButton.addEventListener("click", function() {
   var tbody = document.getElementById("parts-table-body");
-  var rowText = `
-    <th scope="row"><i class="fa fa-minus-circle remove-icon" onclick="removeRow(this)"></i></th>
-    <td><input class="text-input part-input" type="text" name="part-number" oninput="updateRowPartNumber(this)"></td>
-    <td>
-      <input class="num-items" type="number" name="num-items" min="1" value="1" style="width:60px;" oninput="updateRowQuantities(this)">
-    </td>
-    <td>10</td>
-    <td>#</td>
-  `;
-  var row = document.createElement("TR");
-  row.innerHTML = rowText;
+  let row = createRow();
   tbody.appendChild(row);
   let partNumberInput = row.cells[1].children[0];
   partNumberInput.focus();
