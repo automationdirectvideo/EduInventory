dabaacad48a0b7dbe0009bb865bf25a48d13dc8c
rstrozensky <rstrozensky@automationdirect.com>
2019-07-18 11:13:14 -0400

    Change drawings on scanning canvas

--- js/main.js
+++ js/main.js
@@ -27,29 +27,35 @@ var liveStreamConfig = {
     readers: [
       'code_128_reader',
       'code_39_reader'
-    ],
-    debug: {
-      drawBoundingBox: true
-    }
+    ]
   },
   locate: false
 };
 
 Quagga.onProcessed(function(result) {
   var drawingCtx = Quagga.canvas.ctx.overlay, drawingCanvas = Quagga.canvas.dom.overlay;
-
+  var height = parseInt(drawingCanvas.getAttribute("height"));
+  var width = parseInt(drawingCanvas.getAttribute("width"));
+  var box = [
+    [0, height],
+    [0, 0],
+    [width, 0],
+    [width, height],
+  ];
+  Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 5});
+  drawingCtx.clearRect(0, 0, width, height);
   if (result) {
-    if (result.boxes) {
-      // drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-      // result.boxes.filter(function (box) {
-      //   return box !== result.box;
-      // }).forEach(function (box) {
-      //   Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 5});
-      // });
+    // if (result.boxes) {
+    //   drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+    //   result.boxes.filter(function (box) {
+    //     return box !== result.box;
+    //   }).forEach(function (box) {
+    //     Quagga.ImageDebug.drawPath(box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 5});
+    //   });
 
-      drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
-      Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 5});
-    }
+    //   drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+    //   Quagga.ImageDebug.drawPath(result.box, {x: 0, y: 1}, drawingCtx, {color: "green", lineWidth: 5});
+    // }
 
     if (result.codeResult && result.codeResult.code && 
           new Date() - lastSuccessfulScan >= 1000) {
